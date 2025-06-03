85790e6090746cac83e1fe0ecb9db23e4fb26256
HaiDank <danevil2003@gmail.com>
2025-06-03 16:44:08 +0700

    complete lesson 7 web-server express

--- web-server/src/app.ts
+++ web-server/src/app.ts
@@ -1,1 +1,54 @@
-import express from 'express'
+import chalk from 'chalk'
+import express, { Request, Response } from 'express'
+import { create } from 'express-handlebars'
+import path from 'path'
+
+const app = express()
+const hbs = create({ partialsDir: ['./views/partials/'] })
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.get('', (req: Request, res: Response) => {
+	res.render('index', {
+		title: 'Home',
+	})
+})
+
+app.get('/help', (req: Request, res: Response) => {
+	res.render('help', {
+		title: 'Help',
+	})
+})
+
+app.get('/about', (req: Request, res: Response) => {
+	res.render('about', {
+		title: 'About page',
+		name: 'Dank',
+		age: 22,
+	})
+})
+
+app.get('/weather', (req: Request, res: Response) => {
+	res.render('weather', {
+		title: 'Weather',
+		forecast: 'test',
+		location: 'test',
+	})
+})
+
+app.get('/help/{*any}', (req: Request, res: Response) => {
+	res.render('help-404')
+})
+
+app.get('/{*any}', (req: Request, res: Response) => {
+	res.render('404')
+})
+
+app.listen(3000, () => {
+	console.log(path.join(__dirname, '../public'))
+	console.log('Server is up on port: ' + chalk.bold.yellow('http://localhost:3000'))
+})
