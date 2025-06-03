1a7922f8b0e2a75ae6d40e4c3c139f7089d64304
HaiDank <danevil2003@gmail.com>
2025-06-03 11:26:11 +0700

    setting up mapbox api

--- weather-app/src/app.ts
+++ weather-app/src/app.ts
@@ -4,19 +4,24 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const instance = axios.create({
+const weatherAxios = axios.create({
 	baseURL: `https://api.pirateweather.net/forecast/${process.env.API_KEY}/16.0556, 108.2343?units=si`,
 	timeout: 1000,
-	headers: { 'X-Custom-Header': 'foobar' },
 })
 
-instance
+const mapboxAxios = axios.create({
+	baseURL: `https://api.mapbox.com/search/geocode/v6/forward?access_token=${process.env.MAPBOX_TOKEN}&limit=1`,
+	timeout: 1000,
+})
+
+weatherAxios
 	.get('')
 	.then((data) => {
 		const currentData = data.data.currently
 		const dailyData = data.data.daily
-		console.log(dailyData.summary + 
-			' It is currently ' +
+		console.log(
+			dailyData.summary +
+				' It is currently ' +
 				chalk.bold.green(currentData.temperature + '°C') +
 				' out. There is a ' +
 				chalk.bold.green(currentData.precipProbability + '%') +
@@ -24,3 +29,16 @@ instance
 		)
 	})
 	.catch((e) => console.log(chalk.red(e)))
+
+mapboxAxios
+	.get('', {
+        params: {
+            q: 'Los Angeles'
+        }
+    })
+	.then((data) => {{
+        console.log('Latitude: ', data.data.features[0].geometry.coordinates[1])
+		console.log('Longtitude: ', data.data.features[0].geometry.coordinates[0])
+    }
+	})
+	.catch((e) => console.log(e))
