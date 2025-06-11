1090c77694a279d1090b38adb59d7dfa69aeefbb
HaiDank <132982748+HaiDank@users.noreply.github.com>
2025-06-11 11:18:58 +0700

    testing socket integration

--- chat-app/public/javascripts/chat.js
+++ chat-app/public/javascripts/chat.js
@@ -1,1 +1,13 @@
-io();
+const socket = io();
+
+socket.on('message', (msg) => {
+	console.log(msg);
+});
+
+document.querySelector('#chat-form').addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const msg = e.target.elements.message.value;
+
+	socket.emit('sendMessage', msg);
+});
